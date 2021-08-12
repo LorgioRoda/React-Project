6133adf7a5e4e5dd7b5c8d4982325c2b7cc80e40
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-12 10:43:58 -0400

    add customs hooks

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import initialState from '../initialState';
+export default function useInitialState() {
+  const [state, setState] = useState(initialState);
+
+  const addToCart = (payload) => {
+    setState({
+      ...state,
+      cart: [...state, payload],
+    });
+  };
+
+  const remoteToCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((items) => items.id !== payload.id),
+    });
+  };
+  return {
+    addToCart,
+    remoteToCart,
+    state,
+  };
+}
