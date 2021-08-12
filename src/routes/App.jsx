96991477f5fbe71faa431afdeed85475cb0a894d
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-12 10:49:31 -0400

    add context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AppProvider from "../context/AppContext"
 import Home from '../container/Home';
 import Checkout from '../container/Checkout';
 import Information from '../container/Information';
@@ -8,20 +9,23 @@ import Success from '../container/Success';
 import NotFound from '../container/NotFound';
 import Layout from '../components/Layout';
 
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/checkout" component={Checkout} />
-            <Route exact path="/checkout/information" component={Information} />
-            <Route exact path="/checkout/payment" component={Payment} />
-            <Route exact path="/checkout/success" component={Success} />
-            <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/checkout" component={Checkout} />
+              <Route exact path="/checkout/information" component={Information} />
+              <Route exact path="/checkout/payment" component={Payment} />
+              <Route exact path="/checkout/success" component={Success} />
+              <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppProvider>
   );
 };
 
