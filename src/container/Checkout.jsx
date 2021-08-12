c29d2eed03a236783569ca5e4945980805583626
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-11 22:05:36 -0400

    add checkout

--- src/container/Checkout.jsx
+++ src/container/Checkout.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import '../styles/components/Checkout.css';
 
 export default function Checkout() {
-    return (
-        <div>
-            
+  return (
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Wish list:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Delete</button>
         </div>
-    )
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Price Total: $10</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
+    </div>
+  );
 }
