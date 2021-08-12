afea3cdd3e637991eb002836a4418a7fd39b3790
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-12 08:47:06 -0400

    add payment and success components

--- src/container/Payment.jsx
+++ src/container/Payment.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import "../styles/components/Payment.css"
 
 export default function Payment() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resument del pedido:</h3>
+        <div className="Payment-button">Boton de pago con Paypal</div>
+      </div>
+      <div />
+    </div>
+  );
 }
